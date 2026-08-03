5cb3b99110cee111d160b1bc230a069853e8210b
emmamamani-max <emma.mamani@uab.edu.bo>
2026-08-03 00:03:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+
+const menuToggle=document.getElementById("menu-toggle");
+const navLinks=document.getElementById("nav-links");
+
+menuToggle.addEventListener("click",function(){
+
+    navLinks.classList.toggle("active")
+
+});
+
+</script>
